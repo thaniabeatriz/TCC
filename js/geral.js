970fc7a052d3aacd97f582dfc4b7b4a8b8ae0a9a
Thania Beatriz <thaniabeatriz2017@gmail.com>
2024-02-22 09:39:44 -0400

    Criando o script js do botao contraste

---
+++ js/geral.js
@@ -0,0 +1,40 @@
+$( document ).ready(function() {
+
+    // VERIFICA SE BOTÃO DE CONTRASTE ESTÁ ATIVADO NA PÁGINA
+    var verificaContraste = localStorage.getItem("contraste");
+    if (verificaContraste === "1") {
+        definicoesContraste();
+    }
+
+});
+
+function contraste() { //Ao clicar
+
+    /* VERIFICA SE A PESSOA CLICOU NO BOTÃO DE CONTRATE, 
+       CASO ESTEJA JÁ ATIVO, ELE FAZ O REFRESH NA PÁGINA PARA VOLTAR AS CONFIGURAÇÕES PADRÃO */
+    var verificaContraste = localStorage.getItem("contraste");
+    if (verificaContraste === "1") {
+        
+        localStorage.setItem("contraste", "0");
+        location.reload();
+
+    } else {
+        
+        definicoesContraste();
+
+        // Define um cookie de navegador para guardar as informações de contraste
+        localStorage.setItem("contraste", "1");
+    }
+
+}
+
+function definicoesContraste() {
+    $("body").css("background-color","black");
+    $(".header").css("background-color","#1f6d00");
+    $("body").css("color","white");
+    $(".carrossel").css("background-color","black");
+    $(".motivacional").css("background-color","black");
+    $("a").css("color","yellow");
+    $(".botao-contraste").css("background-color","#1f6d00");
+    $(".botao-contribuir").css("background-color","#ff5b0e"); 
+}
